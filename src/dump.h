9f0a75104da58dd1c8a88e825ff3a7815d8ca7f8
sbennett <sbennett1990@users.noreply.github.com>
2014-08-29 14:49:02 -0400

    Change header to C style.

--- src/dump.h
+++ src/dump.h
@@ -1,12 +1,13 @@
-/**
+/*
  * File:   dump.h
  * Author: Scott Bennett
  */
 
 #ifndef DUMP_H
 #define	DUMP_H
+
 void dumpMemory();
 void dumpProgramRegisters();
 void dumpProcessorRegisters();
-#endif	/* DUMP_H */
 
+#endif	/* DUMP_H */
