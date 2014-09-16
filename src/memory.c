3e4b6c0aabdab587b10c8729cf73162869c9fc87
sbennett <sbennett1990@users.noreply.github.com>
2014-09-16 09:50:41 -0400

    memory.c - Convert comments to C style

--- src/memory.c
+++ src/memory.c
@@ -1,4 +1,4 @@
-/**
+/* 
  * File:   memory.c
  * Author: Scott Bennett
  */
@@ -13,13 +13,15 @@ static unsigned int memory[MEMSIZE];
 static unsigned int fetch(int address, bool * memError);
 static void store(int address, unsigned int value, bool * memError);
 
-/**
+/* 
  * Read and return a word of memory. If there is an error,
  * *memError is set to true, otherwise it's false.
  * 
- * @param address    Address of memory [0..1023]
- * @param *memError  Pointer to a memory error indicator
- * @return The contents of memory at address, or 0 for error
+ * Parameters:
+ *  address     the memory address [0..1023]
+ *  *memError   pointer to the memory error indicator
+ * 
+ * Return the contents of the memory address, or 0 on error.
  * 
  * NOTE: This function can not be accessed outside
  * of memory.c
@@ -34,14 +36,15 @@ unsigned int fetch(int address, bool * memError) {
     return memory[address];
 }
 
-/**
+/* 
  * Store a value (1 word) in memory. If there is an error,
  * *memError is set to true, otherwise it's false. If address
  * isn't a valid memory address, then memory isn't modified.
  * 
- * @param address    Address of memory [0..1023]
- * @param value      Value to store in memory at address
- * @param *memError  Pointer to a memory error indicator
+ * Parameters:
+ *  address     the memory address [0..1023]
+ *  value       the value to store in memory at the address
+ *  *memError   pointer to the memory error indicator
  * 
  * NOTE: This function can not be accessed outside
  * of memory.c
@@ -55,13 +58,15 @@ void store(int address, unsigned int value, bool * memError) {
     }
 }
 
-/**
+/* 
  * Read a byte of memory using it's byte address. If there is 
  * an error, *memError is set to true, otherwise it's false.
  * 
- * @param byteAddress  A byte address of memory [0..4095]
- * @param *memError    Pointer to a memory error indicator
- * @return The contents of memory at byte address, or 0 for error
+ * Parameters:
+ *  byteAddress     a byte address of memory [0..4095]
+ *  *memError       pointer to the memory error indicator
+ * 
+ * Return the contents of memory at the byte address, or 0 for error
  */
 unsigned char getByte(int byteAddress, bool * memError) {
     if (byteAddress < 0 || byteAddress > HIGHBYTE) {
@@ -71,21 +76,24 @@ unsigned char getByte(int byteAddress, bool * memError) {
     
     *memError = FALSE;
     
-    // Retrieve the word containing the specified byte address 
-    // using integer division
+    /*
+     * Retrieve the word containing the specified byte address 
+     * using integer division
+     */
     unsigned int word = fetch((byteAddress / WORDSIZE), memError);
     
     return getByteNumber((byteAddress % WORDSIZE), word);
 }
 
-/**
+/* 
  * Store a value (1 byte) in memory. If there is an error,
  * *memError is set to true, otherwise it's false. If address
  * isn't a valid memory address, then memory isn't modified.
  * 
- * @param byteAddress  A byte address of memory [0..4095]
- * @param value        Value to store in memory at address
- * @param *memError    Pointer to a memory error indicator
+ * Parameters:
+ *  byteAddress     a byte address of memory [0..4095]
+ *  value           the value to store in memory at the address
+ *  *memError       pointer to the memory error indicator
  */
 void putByte(int byteAddress, unsigned char value, bool * memError) {
     if (byteAddress < 0 || byteAddress > HIGHBYTE) {
@@ -104,14 +112,16 @@ void putByte(int byteAddress, unsigned char value, bool * memError) {
     }
 }
 
-/**
+/* 
  * Read a word of memory using it's byte address. If there is 
  * an error, *memError is set to true, otherwise it's false.
  * 
- * @param byteAddress  A byte address of memory [0..4095] that is
+ * Parameters:
+ *  byteAddress     a byte address of memory [0..4095] that is
  *                     a multiple of 4
- * @param *memError    Pointer to a memory error indicator
- * @return The contents of memory at byte address, or 0 for error
+ *  *memError       pointer to the memory error indicator
+ * 
+ * Return the contents of memory at the byte address, or 0 on error
  */
 unsigned int getWord(int byteAddress, bool * memError) {
     if (byteAddress % WORDSIZE) { // byteAddress not a multiple of 4
@@ -123,26 +133,27 @@ unsigned int getWord(int byteAddress, bool * memError) {
     return fetch((byteAddress / WORDSIZE), memError);
 }
 
-/**
+/* 
  * Store a value (1 word) in memory. If there is an error,
  * *memError is set to true, otherwise it's false. If address
  * isn't a valid memory address, then memory isn't modified.
  * 
- * @param byteAddress  A byte address of memory [0..4095] that is
+ * Parameters:
+ *  byteAddress     a byte address of memory [0..4095] that is
  *                     a multiple of 4
- * @param value        Value to store in memory at address
- * @param *memError    Pointer to a memory error indicator
+ *  value           the value to store in memory at the address
+ *  *memError       pointer to the memory error indicator
  */
 void putWord(int byteAddress, unsigned int value, bool * memError) {
     if (byteAddress % WORDSIZE) { // byteAddress not a multiple of 4
         *memError = TRUE;
-    } else {  // byteAddress is an acceptable word address
+    } else {    // byteAddress is an acceptable word address
         *memError = FALSE;
         store((byteAddress / WORDSIZE), value, memError);
     }
 }
 
-/**
+/* 
  * Set all memory locations to 0 (effectively
  * "clearing" the memory).
  */
