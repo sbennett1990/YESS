00d6f4845ff5532683dd85d9a03dd59e5895e2ce
sbennett <sbennett1990@users.noreply.github.com>
2014-09-16 13:27:44 -0400

    loader.h - update the macro by adding parentheses

--- src/loader.h
+++ src/loader.h
@@ -4,8 +4,8 @@
  */
 
 // define's
-#define WHICH_BYTE(n)   ((n * 2) + 7)   // Calculate the column of the data byte
-#define HEX   16   // Hexidecimal base number
+#define WHICH_BYTE(n)   (((n) * 2) + 7)	// Calculate the column of the data byte
+#define HEX				16				// Hexidecimal base number
 
 #ifndef LOADER_H
 #define	LOADER_H
