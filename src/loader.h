89a3f0a84fe74905749e952d3333d1c6b5fe0543
sbennett <sbennett1990@users.noreply.github.com>
2014-09-16 15:32:53 -0400

    Update loader.h

--- src/loader.h
+++ src/loader.h
@@ -5,7 +5,7 @@
 
 // define's
 //#define WHICH_BYTE(n)	(((n) * 2) + 7)	// Calculate the column of the data byte
-#define HEX				16				// Hexidecimal base number
+#define HEX		16		// Hexidecimal base number
 
 #ifndef LOADER_H
 #define	LOADER_H
