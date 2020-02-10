ffdd0ed541e407f0364dbf61aec3c601f03210b6
Scott Bennett <scottb@fastmail.com>
2020-02-10 16:39:53 -0500

    Style tweaks

--- src/registers.h
+++ src/registers.h
@@ -2,23 +2,25 @@
  * registers.h
  */
 
-#ifndef REGISTERS_H
-#define REGISTERS_H
+#ifndef	REGISTERS_H
+#define	REGISTERS_H
 
-#define REGSIZE    8     /* number of registers */
-#define EAX      0x0
-#define ECX      0x1
-#define EDX      0x2
-#define EBX      0x3
-#define ESP      0x4
-#define EBP      0x5
-#define ESI      0x6
-#define EDI      0x7
-#define RNONE    0xf     /* i.e. - no register needed */
-/* condition codes (CC) */
-#define ZF       0x2     /* zero flag     - bit 2 of the CC */
-#define SF       0x1     /* sign flag     - bit 1 of the CC */
-#define OF       0x0     /* overflow flag - bit 0 of the CC */
+#define REGSIZE	8	/* number of registers */
+/* Program Registers */
+#define EAX		0x0
+#define ECX		0x1
+#define EDX		0x2
+#define EBX		0x3
+#define ESP		0x4
+#define EBP		0x5
+#define ESI		0x6
+#define EDI		0x7
+#define RNONE	0xf	/* i.e. - no register needed */
+
+/* Condition Codes (CC) */
+#define ZF		0x2		/* zero flag     - bit 2 of the CC */
+#define SF		0x1		/* sign flag     - bit 1 of the CC */
+#define OF		0x0		/* overflow flag - bit 0 of the CC */
 
 void clearCC(void);
 void clearRegisters(void);
