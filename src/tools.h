d13174fb120586f2b6ffcb234166c93f6f53e8c7
Scott Bennett <scottb@fastmail.com>
2020-02-10 12:14:26 -0500

    Style tweak

--- src/tools.h
+++ src/tools.h
@@ -1,13 +1,12 @@
 /*
  * File:   tools.h
- * Author: Scott Bennett
  */
 
 #ifndef TOOLS_H
 #define TOOLS_H
 
-#define BYTESIZE   8  // Size of one byte in bits
-#define INTHIGHBIT 31 // Highest bit for type int
+#define BYTESIZE	8   // Size of one byte in bits
+#define INTHIGHBIT	31  // Highest bit for type int
 
 #include <stddef.h>
 #include "bool.h"
@@ -17,10 +16,10 @@ unsigned int setBits(int low, int high, unsigned int source);
 unsigned int clearBits(int low, int high, unsigned int source);
 unsigned int assignOneBit(int bitNumber, int bitValue, unsigned int source);
 unsigned char getByteNumber(int byteNo, unsigned int source);
-unsigned int putByteNumber(int byteNo, unsigned char byteValue, 
-                           unsigned int source);
-unsigned int buildWord(unsigned char byte0, unsigned char byte1, 
-                       unsigned char byte2, unsigned char byte3);
+unsigned int putByteNumber(int byteNo, unsigned char byteValue,
+    unsigned int source);
+unsigned int buildWord(unsigned char byte0, unsigned char byte1,
+    unsigned char byte2, unsigned char byte3);
 bool isNegative(unsigned int source);
 void expandBits(unsigned int source, char bits[36]);
 void clearBuffer(char * buff, size_t length);
@@ -30,4 +29,3 @@ void initialpledge(void);
 void reduceprivileges(void);
 
 #endif /* TOOLS_H */
-
