c13c4bda8ffbe9c589ae9c0a472f046bffe656fb
Scott Bennett <scottb@fastmail.com>
2020-07-03 22:27:25 -0400

    Add my copyright to files I definitely remember writing

--- src/loader.c
+++ src/loader.c
@@ -1,6 +1,20 @@
 /*
- * loader.c
+ * Copyright (c) 2014, 2016, 2020 Scott Bennett <scottb@fastmail.com>
  *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+/*
  * Loads machine source code (in ASCII form) into YESS memory. Also performs
  * error checking on the source file.
  */
