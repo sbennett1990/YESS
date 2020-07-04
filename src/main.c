c76f8daa91a8479a1343dbd1156e2a7c2b5cba66
Scott Bennett <scottb@fastmail.com>
2020-07-03 22:09:35 -0400

    spaces -> tabs

--- src/main.c
+++ src/main.c
@@ -27,8 +27,8 @@
 static void
 usage(void)
 {
-    fprintf(stderr, "usage: yess [-duv] -f <filename>.yo\n");
-    exit(EXIT_FAILURE);
+	fprintf(stderr, "usage: yess [-duv] -f <filename>.yo\n");
+	exit(EXIT_FAILURE);
 }
 
 /*
@@ -38,97 +38,97 @@ usage(void)
 static void
 setupyess(void)
 {
-    /* initialize function pointer array */
-    initFuncPtrArray();
+	/* initialize function pointer array */
+	initFuncPtrArray();
 
-    clearMemory();
-    clearFregister();
-    clearDregister();
-    clearEregister();
-    clearMregister();
-    clearWregister();
+	clearMemory();
+	clearFregister();
+	clearDregister();
+	clearEregister();
+	clearMregister();
+	clearWregister();
 }
 
 /*
  * Main
  */
 int
-main(int argc, char ** argv)
+main(int argc, char **argv)
 {
-    int ch;
-    int dflag = 0;	/* debug */
-    int vflag = 0;	/* verbose */
-    int verbosity = 0;
-    const char * sourcefile;
+	int ch;
+	int dflag = 0;	/* debug */
+	int vflag = 0;	/* verbose */
+	int verbosity = 0;
+	const char *sourcefile;
 
-    while ((ch = getopt(argc, argv, "df:uv")) != -1) {
-        switch (ch) {
-            case 'd':
-                dflag = 1;
-                break;
-            case 'f':
-                sourcefile = optarg;
-                break;
-            case 'u':
-                usage(); /* EXIT */
-            case 'v':
-                vflag = 1;
-                break;
-            default:
-                usage();
-                /* NOTREACHED */
-        }
-    }
-    argc -= optind;
-    argv += optind;
+	while ((ch = getopt(argc, argv, "df:uv")) != -1) {
+		switch (ch) {
+		case 'd':
+			dflag = 1;
+			break;
+		case 'f':
+			sourcefile = optarg;
+			break;
+		case 'u':
+			usage(); /* EXIT */
+		case 'v':
+			vflag = 1;
+			break;
+		default:
+			usage();
+			/* NOTREACHED */
+		}
+	}
+	argc -= optind;
+	argv += optind;
 
-    if (argc > 0 || sourcefile == NULL) {
-        usage(); /* EXIT */
-    }
+	if (argc > 0 || sourcefile == NULL) {
+		usage(); /* EXIT */
+	}
 
-    if (vflag) {
-        verbosity = 1;
-    }
+	if (vflag) {
+		verbosity = 1;
+	}
 
-    /* max verbosity, with or without -v flag */
-    if (dflag) {
-        verbosity = 2;
-    }
+	/* max verbosity, with or without -v flag */
+	if (dflag) {
+		verbosity = 2;
+	}
 
-    /* done with option parsing, initialize the program */
-    log_init(verbosity, 0);
-    log_debug("initializing YESS...");
-    initialpledge();
+	/* done with option parsing, initialize the program */
+	log_init(verbosity, 0);
+	log_debug("initializing YESS...");
+	initialpledge();
 
-    /* initialize the 'processor' */
-    setupyess();
+	/* initialize the 'processor' */
+	setupyess();
 
-    /* load 'program' file into 'memory'; terminate if there is a problem */
-    if (!load(sourcefile)) {
-        dumpMemory();
-        log_warn("error loading the file");
-        log_debug("exiting");
-        return 1; /* EXIT */
-    }
+	/* load 'program' file into 'memory'; terminate if there is a problem */
+	if (!load(sourcefile)) {
+		dumpMemory();
+		log_warn("error loading the file");
+		log_debug("exiting");
+		return 1; /* EXIT */
+	}
 
 	/* reduce privileges */
-    reduceprivileges();
+	reduceprivileges();
 
 	/* execute the 'program' */
-    int clockCount = 0;
-    int stop = 0;
-    forwardType forward;
-    statusType status;
-    controlType control;
+	int clockCount = 0;
+	int stop = 0;
+	forwardType forward;
+	statusType status;
+	controlType control;
 
-    while (stop != -1) {
-        stop = writebackStage(&forward, &status);
-        memoryStage(&forward, &status, &control);
-        executeStage(&forward, status, &control);
-        decodeStage(forward, &control);
-        fetchStage(forward, control);
-        clockCount++;	/* each loop iteration is 1 clock cycle */
-    }
+	while (stop != -1) {
+		stop = writebackStage(&forward, &status);
+		memoryStage(&forward, &status, &control);
+		executeStage(&forward, status, &control);
+		decodeStage(forward, &control);
+		fetchStage(forward, control);
+		clockCount++;	/* each loop iteration is 1 clock cycle */
+	}
 
 	/* XXX: need to not dump if error encountered */
 #if 0
