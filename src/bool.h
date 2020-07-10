ac44a15121785939eca2294b27f774eae05c0e60
Scott Bennett <scottb@fastmail.com>
2020-07-09 21:29:03 -0400

    spaces -> tabs

--- src/bool.h
+++ src/bool.h
@@ -2,13 +2,12 @@
  * bool.h
  */
 
-#ifndef BOOL_H
-#define BOOL_H
+#ifndef	BOOL_H
+#define	BOOL_H
 
-#define TRUE   1
-#define FALSE  0
+#define TRUE	1
+#define FALSE	0
 
 typedef int bool;
 
-#endif  /* BOOL_H */
-
+#endif	/* BOOL_H */
