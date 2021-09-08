6f0514524b822b98f0f1f1da9bd633f4232accb6
Scott Bennett <scottb@fastmail.com>
2021-09-07 20:38:42 -0400

    Better commentary

--- src/memory.h
+++ src/memory.h
@@ -21,19 +21,19 @@
 
 #include "bool.h"
 
-#define MEMSIZE     1024	// 1024 words of memory
-#define WORDSIZE       4	// Size of one word in bytes
-#define HIGHBYTE    4095	// Highest byte address in memory
-#define NOADDRESS      0	// Value for a "no address"
+#define MEMSIZE     1024	/* Total number of words of memory */
+#define WORDSIZE       4	/* Size of one word in bytes */
+#define HIGHBYTE    4095	/* Highest byte address in memory */
+#define NOADDRESS      0	/* Value for "not a memory address" */
 
 void initMemory(void);
 void destroyMemory(void);
 void clearMemory(void);
 
 uint8_t getByte(int address, bool *memError);
-void putByte(int address, uint8_t value, bool *memError);
+void putByte(int address, uint8_t data, bool *memError);
 
-unsigned int getWord(int address, bool * memError);
-void putWord(int address, unsigned int value, bool * memError);
+unsigned int getWord(int address, bool *memError);
+void putWord(int address, unsigned int data, bool *memError);
 
 #endif	/* MEMORY_H */
